cb5185147d4549ecc19590bee9058f4d21860a83
Keila-Benitez <144249084+Keila-Benitez@users.noreply.github.com>
2023-09-12 18:50:14 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+console.log("Hola Mundo")
+
+let intentos = 6;
+let palabra = "APPLE";
+
+const button = document.getElementById("guess-button");
+
+button.addEventListener("click", intentar);
+
+function intentar(){
+    const INTENTO = leerIntento();
+        if (INTENTO === palabra ) {
+            console.log("GANASTE!")
+            return
+        }
+        for (let i in palabra){
+            if (INTENTO[i]===palabra[i]){
+                console.log(INTENTO[i], "VERDE")
+            } else if( palabra.includes(INTENTO[i]) ) {
+                console.log(INTENTO[i], "AMARILLO")
+            } else {
+                console.log(INTENTO[i], "GRIS")
+            }
+        }
+            intentos--
+        if (intentos==0){
+            console.log("PERDISTE!")
+        }
+    }
+
+function leerIntento(){
+    let intento = document.getElementById("guess-input");
+    intento = intento.value;
+    intento = intento.toUpperCase();
+
+    return intento;
+}
